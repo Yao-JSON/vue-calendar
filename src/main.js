25d7bff9e33843fddc42c3879634a852e115a3b7
姚嘉松 <yaojiasong@youzan.com>
2019-05-23 17:18:44 +0800

    fix: 更新 webpack 配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import App from './App';
 
-Vue.config.productionTip = false
+import './../static/index.css'
 
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  components: { 'my-canlendar': App },
+  render(h) {
+    return h('my-canlendar');
+  },
+});
